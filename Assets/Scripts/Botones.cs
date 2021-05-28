af9207eadb11b2c035f5e02ea4084b3ae509ec29
LiamTapia <36176046+LiamTapia@users.noreply.github.com>
2021-05-27 17:27:04 -0700

    Se agrego Watson para la voz, se arreglaron detalles pero los aspectos graficos cambiados fueron perdidos en un crasheo. Viviran siempre en mi memoria.

---
+++ Assets/Scripts/Botones.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Botones : MonoBehaviour
+{
+    public float TimetoAct = 5f;
+    public float TimeCounter = 0f;
+    public bool isGazed = false;
+    public bool ActiveState = false;
+    public int buttonType;
+    public GameObject MenuOpciones;
+    public AudioSource audioButton;
+
+    public void SetGazedAt(bool gazedAt)
+    {
+        audioButton.Play();
+        Debug.Log("Me estan mirando");
+        isGazed = gazedAt;
+
+        if(!gazedAt)
+            TimeCounter = 0f;
+
+    }
+
+    public void DoSomething()
+    {
+        Debug.Log("Hice algo");
+        
+        switch(buttonType){
+            case 0:
+                IniciarNivel();
+                break;
+            case 1: 
+                SalirApp();
+                break;
+            case 2:
+                Reanudar();
+                break;
+            case 3: 
+                SalirNivel();
+                break;
+        }
+
+        
+    }
+
+    public void IniciarNivel()
+    {
+        SceneManager.LoadScene("Nivel1");
+    }
+
+    public void SalirApp()
+    {
+        Application.Quit();
+    }
+
+    public void Reanudar()
+    {
+        MenuOpciones.SetActive(false);
+    }
+
+    public void SalirNivel()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    
+
+    public void Update()
+    {
+        if(isGazed && !ActiveState)
+        {
+            if((TimeCounter += Time.deltaTime) >= TimetoAct)
+            {
+                DoSomething();
+                TimeCounter = 0f;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/VoiceCommandPausa.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityStandardAssets.CrossPlatformInput;
+using IBM.Watsson.Examples;
+public class VoiceCommandPausa : MonoBehaviour
+{
+    public string voiceCommand = "start";
+    public GameObject menu;
+
+    void Awake() 
+    {
+        VoiceCommandProcessor commandProcessor = GameObject.FindObjectOfType<VoiceCommandProcessor>();
+        //commandProcessor.onVoiceCommandRecognized += OnVoiceCommandRecognized;
+    }
+
+    public void OnVoiceCommandRecognized(string command)
+    {
+        if(command.ToLower().Contains(voiceCommand.ToLower()))
+        {
+            menu.SetActive(true);
+        }
+    }
+}
